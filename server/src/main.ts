86719da806e80476ffb87505bf0e9aa1a6f872af
dong53358 <dong53358@naver.com>
2025-04-05 21:31:20 +0900

    feat: swagger 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
+    // 스웨거 설정
+    const config = new DocumentBuilder()
+        .setTitle('API 문서')
+        .setDescription('API에 대한 설명')
+        .setVersion('1.0')
+        .addTag('API')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document)
+
     await app.listen(3000)
 }
 bootstrap()
--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { User } from './entity/users.entity'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get()
+    @ApiOperation({ summary: '모든 사용자 조회' })
+    @ApiResponse({ status: 200, description: '사용자 목록 반환', type: [User] })
     findAll(): Promise<User[]> {
         return this.usersService.findAll()
     }
