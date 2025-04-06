0723f48bf8a66e353b070c20c04d25cf4b29ab4b
dong53358 <dong53358@naver.com>
2025-04-06 23:09:43 +0900

    feat: cors 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,11 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
+    app.enableCors({
+        origin: 'http://localhost:3001', // 프론트엔드 URL
+        credentials: true, // 인증 정보(쿠키) 허용
+    })
+
     // 쿠키 파서 미들웨어 추가
     app.use(cookieParser())
 
