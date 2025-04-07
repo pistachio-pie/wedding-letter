f342743e069a3e7034f4e74710bd21e891ff0982
dong53358 <dong53358@naver.com>
2025-04-07 21:19:07 +0900

    feat: AccessTokenGuard 수정

--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -10,17 +10,16 @@ import {
 import { UsersService } from './users.service'
 import { User } from './entity/users.entity'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
-import { JwtAuthGuard } from 'src/guard/jwt-auth.guard'
 import { AccessTokenGuard } from 'src/guard/access-token.guard'
 
-@ApiTags('Users')
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
-    @Get('me')
-    @UseGuards(JwtAuthGuard)
-    @ApiOperation({ summary: '현재 인증된 사용자 정보 조회 (Bearer 토큰)' })
+    @Get('profile')
+    @UseGuards(AccessTokenGuard)
+    @ApiOperation({ summary: '현재 인증된 사용자 정보 조회 (쿠키 토큰)' })
     @ApiResponse({
         status: 200,
         description: '인증된 사용자 정보 반환',
@@ -31,20 +30,8 @@ export class UsersController {
         return req.user
     }
 
-    @Get('me/cookie')
-    @UseGuards(AccessTokenGuard)
-    @ApiOperation({ summary: '현재 인증된 사용자 정보 조회 (쿠키 토큰)' })
-    @ApiResponse({
-        status: 200,
-        description: '인증된 사용자 정보 반환',
-        type: User,
-    })
-    getProfileFromCookie(@Req() req) {
-        // AccessTokenGuard와 AccessTokenStrategy에 의해 검증된 사용자 정보가 req.user에 담깁니다.
-        return req.user
-    }
-
     @Get()
+    @UseGuards(AccessTokenGuard)
     @ApiOperation({ summary: '모든 사용자 조회' })
     @ApiResponse({ status: 200, description: '사용자 목록 반환', type: [User] })
     findAll(): Promise<User[]> {
@@ -52,11 +39,13 @@ export class UsersController {
     }
 
     @Get(':id')
+    @UseGuards(AccessTokenGuard)
     findOne(@Param('id') id: string): Promise<User | undefined> {
         return this.usersService.findOne(id)
     }
 
     @Post()
+    @UseGuards(AccessTokenGuard)
     create(@Body() user: User): Promise<User> {
         return this.usersService.create(user)
     }
