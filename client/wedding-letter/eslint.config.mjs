5b2b7bb31714286e407debe11d52591851032273
Kim YoonJung <codekyz0111@gmail.com>
2025-04-11 16:13:32 +0900

    ESLint 설정파일 관련 문제 해결 시도

--- client/wedding-letter/eslint.config.mjs
+++ client/wedding-letter/eslint.config.mjs
@@ -1,11 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url"; import { FlatCompat } from "@eslint/eslintrc";
-const
-_filename = fileURLToPath (import.meta.url);
-const _dirname = dirname(_filename);
-const compat = new FlatCompat ( {
-baseDirectory: _dirname,
-});
-export default I
-...compat. extends ("next/core-web-vitals")
-1: 
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+
+export default eslintConfig
