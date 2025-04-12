b192df9299f8fbf47b87c24d3835bbf75d3d6c0f
dong53358 <dong53358@naver.com>
2025-04-12 20:36:36 +0900

    feat: user name 임의로 지정

--- server/src/modules/auth/kakao.strategy.ts
+++ server/src/modules/auth/kakao.strategy.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-kakao'
 import { ConfigService } from '@nestjs/config'
 import { AuthService } from './auth.service'
+import { v4 as uuidv4 } from 'uuid'
 
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
@@ -25,9 +26,32 @@ export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
         const { id, username, _json } = profile
         const { kakao_account } = _json
 
+        // 이름이 없는 경우 대체 이름 생성
+        let userName = kakao_account?.profile?.nickname || username
+
+        if (!userName || userName === '미연동계정') {
+            // 임의의 한글 이름 배열
+            const randomNames = [
+                '하늘',
+                '바다',
+                '산',
+                '구름',
+                '별',
+                '꽃',
+                '나무',
+                '햇살',
+                '달빛',
+                '바람',
+            ]
+            const randomName =
+                randomNames[Math.floor(Math.random() * randomNames.length)]
+            const shortUuid = uuidv4().substring(0, 6)
+            userName = `${randomName}${shortUuid}`
+        }
+
         const user = {
             id: id,
-            name: kakao_account?.profile?.nickname || username,
+            name: userName,
             email: kakao_account?.email,
             provider: 'kakao',
             kakaoAccessToken: accessToken,
